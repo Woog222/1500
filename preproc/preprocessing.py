d2b4e11b6e74052e949c2a23c907ef285b4d0da7
YunaKim <kya162525@snu.ac.kr>
2023-07-25 16:30:25 +0900

    preproc column name

--- preproc/preprocessing.py
+++ preproc/preprocessing.py
@@ -16,9 +16,15 @@ def preprocessing():
         orders['하차가능시간_시작'] = 60 * orders['하차가능시간_시작'].str[:-3].astype('Int64')
         orders['하차가능시간_종료'] = 60 * orders['하차가능시간_종료'].str[:-3].astype('Int64')
         orders.Group = ((orders.date.str[-1]).astype('Int64') - 1) * 4 + orders.Group
+
+        # 칼럼 순서 맞추기
         orders = orders[
             ['주문ID', '하차지_위도', '하차지_경도', '터미널ID', '착지ID', 'CBM', '하차가능시간_시작', '하차가능시간_종료', '하차작업시간(분)', 'Group']]
+        od_matrix = od_matrix[['origin', 'Destination', 'Distance_km', 'Time_minute']]
+        veh_table = veh_table[['VehNum', 'VehTon', 'BusinessStartTM', 'BusinessEndTM', 'MaxCapaCBM', 'StartCenter', 'FixedCost', 'VariableCost']]
+        terminals = terminals[['ID', 'Origin_Lat', 'Origin_Lon', '상차지권역']]
 
+        # 가공 데이터 저장
         directory = 'data'
         terminals.to_csv(os.path.join(directory, 'terminals.txt'), sep=' ', index=False, header=False)
         od_matrix.to_csv(os.path.join(directory, 'od_matrix.txt'), sep=' ', index=False, header=False)
